ab5e1e98b0c31abb8881fbcbc1f52a6c36b14a84
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 00:42:38 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,12 @@
-class Patient
+class Appointment
+  attr_accessor :patient, :doctor
+
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
     save
   end
 
@@ -14,15 +18,4 @@ class Patient
     @@all
   end
 
-  def new_appointment(date, doctor)
-    Appointment.new(date, self, doctor)
-  end
-
-  def appointments
-    Appointment.all.select { |appt| appt.patient == self }
-  end
-
-  def doctors
-    appointments.map(&:doctor)
-  end
 end
