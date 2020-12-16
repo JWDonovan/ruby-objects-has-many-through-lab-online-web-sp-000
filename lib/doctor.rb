483677126b1d50050249ba2cf19cfd7088df1d4c
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 00:41:39 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,6 +5,10 @@ class Doctor
 
   def initialize(name)
     @name = name
+    save
+  end
+
+  def save
     @@all << self
   end
 
@@ -12,12 +16,12 @@ class Doctor
     @@all
   end
 
-  def new_appointment(patient, date)
+  def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
 
   def appointments
-    Appointment.all.select { |appointment| appointment.doctor == self }
+    Appointment.all.select { |appt| appt.doctor == self }
   end
 
   def patients
