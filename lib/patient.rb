c85e068f50d0e6360b57ee1f251ecee148392000
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 00:45:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,9 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select { |appt| appt.patient == self }
+    Appointment.all.select do |appointment|
+      appointment.patient === self
+    end
   end
 
   def doctors
