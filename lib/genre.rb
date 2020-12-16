da9b612935c00856690d9d5978e757fb97651c7f
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 00:09:47 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -13,7 +13,10 @@ class Genre
   end
 
   def songs
-    Song.all { |song| song.genre == self }
+    #Song.all { |song| song.genre == self }
+    Song.all.select do |song|
+      song.genre === self
+    end
   end
 
   def artists
