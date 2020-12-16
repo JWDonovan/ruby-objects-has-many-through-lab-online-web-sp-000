2d8da502c4387a82fb5780b63cbb018bca0738f8
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 00:41:27 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,21 @@
 class Appointment
-  attr_accessor :date, :doctor, :patient
+  attr_accessor :patient, :doctor
 
   @@all = []
 
   def initialize(date, patient, doctor)
     @date = date
-    @doctor = doctor
     @patient = patient
+    @doctor = doctor
+    save
+  end
+
+  def save
     @@all << self
   end
 
   def self.all
     @@all
   end
+
 end
